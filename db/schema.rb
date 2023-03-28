1b008f3e5f1db2be3a126a030e8dc69c13091d54
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 04:34:20 +0600

    Add post ref comments in schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_222925) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_223207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_222925) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
@@ -42,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_222925) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end
