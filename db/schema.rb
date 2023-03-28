02762a6705e4c3f56bab6c40afc0dc891400b6ba
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 04:28:20 +0600

    Add comments in schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_222341) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_222730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
