4cd9cc9e7fa512295cbc4cd92f5543bc0cc5f10b
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 04:23:02 +0600

    Update schema with user ref to posts using user_id.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_28_222013) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_28_222134) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_222013) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_222013) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
