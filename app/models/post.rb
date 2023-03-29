a1ab76a483de7c3358dd76627486bfa3bf35a443
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 22:37:29 +0600

    Add increment_user_posts_counter method that updates the posts counter for a user.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  after_create :increment_user_posts_counter
+  
+  private
+
+  def increment_user_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
 end
