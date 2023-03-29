051d8047d0518d1347520a191a3dc48459c0ab9d
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 22:46:33 +0600

    Add increment_post_likes_counter method which update likes_counter for a post on new like create.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :increment_post_likes_counter
+
+  private
+
+  def increment_post_likes_counter
+    post.update(likes_counter: post.likes.count)
+  end
 end
