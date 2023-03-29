288b368a6c72eda05d44e239fcfb91118a2d6d02
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 22:24:26 +0600

    model class for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
