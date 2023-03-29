8d5d8e9da0d873b8660271cfb33b707d78a32959
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-29 22:30:41 +0600

    Add recent_posts method for each user which show recent 3 post of that user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
