c0e463c09fdace3f6f501f57d0ed9eb239813416
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-30 21:35:15 +0600

    Update user table according to migration file.

--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_223533) do
     t.string "name"
     t.string "photo"
     t.string "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
