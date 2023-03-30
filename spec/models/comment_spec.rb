ee1daac47326df327960da7bb327d3e9742e854b
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-30 22:58:07 +0600

    Add some additional test cases for Comment class validation.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -14,26 +14,45 @@ RSpec.describe Comment, type: :model do
 
     it 'validates the presence of user when post and text exist' do
       comment = Comment.new(post:, text: 'Test comment')
+
       expect(comment).to_not be_valid
       expect(comment.errors[:user]).to include('must exist')
     end
 
     it 'validates the presence of post when user exist' do
       comment = Comment.new(user:)
+
       expect(comment).to_not be_valid
       expect(comment.errors[:post]).to include('must exist')
     end
 
     it 'validates the presence of post when user and text exist' do
       comment = Comment.new(user:, text: 'Test comment')
+
       expect(comment).to_not be_valid
       expect(comment.errors[:post]).to include('must exist')
     end
 
     it 'validates the presence of text when user and post exist' do
       comment = Comment.new(user:, post:)
+
       expect(comment).to_not be_valid
       expect(comment.errors[:text]).to include("can't be blank")
+    end
+
+    it 'validates that one user can comment when user, post and text exist' do
+      comment = Comment.new(user:, post:, text: 'Test comment')
+
+      expect(comment).to be_valid
+    end
+
+    it 'validates that one user can comment multiple times in same post when user, post and text exist' do
+      comment = Comment.create(text: 'Test comment', user:, post:)
+      comment2 = Comment.create(text: 'Test comment 2', user:, post:)
+
+      expect(comment).to be_valid
+      expect(comment2).to be_valid
+      expect(post.comments).to include(comment, comment2)
     end
   end
 end
