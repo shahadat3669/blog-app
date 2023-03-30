8f4711652fcd84c53641f745844476c26ccb1e53
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-30 21:39:24 +0600

    Add test case for recent_posts method in User class.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -27,4 +27,44 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
   end
+
+  # method tests
+  describe '#recent_posts' do
+    it 'returns empty if the user has no posts' do
+      expect(subject.recent_posts).to eql([])
+    end
+
+    it 'returns the 3 most recent posts for the user if user has more than 3 post' do
+      old_post = subject.posts.create(title: 'Older Post', text: 'Content', created_at: 1.day.ago)
+      new_post = subject.posts.create(title: 'Newer Post', text: 'Content')
+      new_post2 = subject.posts.create(title: 'Newest Post', text: 'Content', created_at: 1.hour.ago)
+      new_post3 = subject.posts.create(title: 'Newest Post', text: 'Content', created_at: 2.hour.ago)
+
+      recent_posts = subject.recent_posts
+
+      expect(recent_posts).to eq([new_post, new_post2, new_post3])
+      expect(recent_posts.size).to eq(3)
+      expect(recent_posts).to_not include(old_post)
+    end
+
+    it 'returns the 3 most recent posts for the user if user has 3 post only' do
+      new_post = subject.posts.create(title: 'Newer Post', text: 'Content')
+      new_post2 = subject.posts.create(title: 'Newest Post', text: 'Content', created_at: 1.hour.ago)
+      new_post3 = subject.posts.create(title: 'Newest Post', text: 'Content', created_at: 2.hour.ago)
+
+      recent_posts = subject.recent_posts
+
+      expect(recent_posts).to eq([new_post, new_post2, new_post3])
+      expect(recent_posts.size).to eq(3)
+    end
+
+    it 'returns less than 3 posts if the user has less than 3 posts' do
+      new_post = subject.posts.create(title: 'Post', text: 'Content')
+
+      recent_posts = subject.recent_posts
+
+      expect(recent_posts).to eq([new_post])
+      expect(recent_posts.size).to eq(1)
+    end
+  end
 end
