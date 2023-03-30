0396ee90691cf616b949fdb24f6b6a3a0c21c221
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-30 23:06:04 +0600

    Add test cases for comments_counter method in comment class.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Comment, type: :model do
   let(:user) { User.create(name: 'Shahadat Hossain', photo: 'https://images.unsplash.com/photo-1651684215020-f7a5b6610f23?&fit=crop&w=640', bio: 'A skilled carpenter with over 15 years of experience, specializing in custom furniture design and installation.') }
   let(:post) { Post.create(title: 'First Post', text: 'First Content', author: user) }
 
+  # validation tests
   describe 'validations:' do
     it 'validates the presence of user when post exist' do
       comment = Comment.new(post:)
@@ -53,6 +54,26 @@ RSpec.describe Comment, type: :model do
       expect(comment).to be_valid
       expect(comment2).to be_valid
       expect(post.comments).to include(comment, comment2)
+    end
+  end
+
+  # method tests
+  describe 'callbacks:' do
+    it 'returns empty if the post has no comment' do
+      expect(post.comments_counter).to eq(0)
+    end
+
+    it 'increments the post comments counter after creating a new comment' do
+      Comment.create(text: 'Test comment', user:, post:)
+
+      expect(post.reload.comments_counter).to eq(1)
+    end
+
+    it 'increment the post comments counter for creating two comment with same user and same post' do
+      Comment.create(text: 'Test comment', user:, post:)
+      Comment.create(text: 'Test comment2', user:, post:)
+
+      expect(post.reload.comments_counter).to eq(2)
     end
   end
 end
