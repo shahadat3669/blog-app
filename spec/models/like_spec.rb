d2f8d623dd47295cc03cbad91361c1f0de8c5e0d
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-30 22:10:13 +0600

    Add test cases for likes_counter method in Like class.

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Like, type: :model do
   let(:post2) { Post.create(title: 'Second Post', text: 'Second Content', author: user) }
   let!(:like) { Like.create(user:, post:) }
 
+  # validation tests
   describe 'validations:' do
     it 'validates the presence of user' do
       like2 = Like.new(post:)
@@ -28,6 +29,35 @@ RSpec.describe Like, type: :model do
       expect(like2).to_not be_valid
       expect(like2.errors[:user_id]).to include('can only like a post once')
       expect(like3).to be_valid
+    end
+  end
+
+  # method tests
+  describe 'callbacks:' do
+    it 'returns empty if the post has no likes' do
+      expect(post2.likes_counter).to eq(0)
+    end
+
+    it 'increments the post likes counter after creating a new like' do
+      Like.create(user:, post: post2)
+
+      expect(post2.reload.likes_counter).to eq(1)
+    end
+
+    it 'Not increment the post likes counter for creating two like with same user and post' do
+      Like.create(user:, post: post2)
+      Like.create(user:, post: post2)
+
+      expect(post2.reload.likes_counter).to eq(1)
+    end
+
+    it 'increment the post likes counter for creating like with different users and same post' do
+      user2 = User.create(name: 'Rakibul Islam', photo: 'https://images.unsplash.com/photo-1530268729831-4b0b9e170218',
+                          bio: 'bio')
+      Like.create(user:, post: post2)
+      Like.create(user: user2, post: post2)
+
+      expect(post2.reload.likes_counter).to eq(2)
     end
   end
 end
