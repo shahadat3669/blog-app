734b3c2da5669809b059a3afd47508fd0f472ac1
Shahadat Hossain <shahadat3669@gmail.com>
2023-03-30 23:15:55 +0600

    Update schema to default value zero for comments_counter and likes_counter attribute in Post table.

--- db/schema.rb
+++ db/schema.rb
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_28_223533) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
