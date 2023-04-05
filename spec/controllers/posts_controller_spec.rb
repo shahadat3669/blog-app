b3f819d9dc08f7209497ce421aa322fb0715eb8e
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-06 02:34:43 +0600

    Add test for #show method in post controller.

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe PostsController, type: :request do
       expect(response.body).to include('Posts#index')
     end
   end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes placeholder text in the response body' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('Posts#show')
+    end
+  end
 end
