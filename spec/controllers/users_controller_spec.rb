d4c52d4257f5ada9b3b45d01891b65997d1b6520
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-06 02:35:37 +0600

    Add test for show method in user controller.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe UsersController, type: :request do
       expect(response.body).to include('Users#index')
     end
   end
+
+  describe 'GET #show' do
+    it 'returns http success' do
+      get '/users/1'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the show template' do
+      get '/users/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'includes the correct placeholder text in the response body' do
+      get '/users/1'
+      expect(response.body).to include('Users#show')
+    end
+  end
 end
