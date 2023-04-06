7482b00e3c7da625dde91e962ceb1cce74d3ec2b
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-07 00:21:11 +0600

    Add :new in route in posts.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new]
   end
 end
