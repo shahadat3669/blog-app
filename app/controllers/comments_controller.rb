47f404a6bd9ed4813d77b38be9995144b7916775
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-07 22:27:40 +0600

    Add create method in CommentsController.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,25 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new
   end
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user = @current_user
+    respond_to do |format|
+      if @comment.save
+        format.html do
+          redirect_to user_post_path(@post.author, @post), notice: 'Comment was successfully created.'
+        end
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
