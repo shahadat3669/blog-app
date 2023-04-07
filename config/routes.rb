4bccc9ba73182246304024a31930a89011858d8c
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-07 22:29:30 +0600

    Update routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :likes, only: [:new, :create]
+    end
+  end
+  resources :posts do
+    resources :comments, only: [:new, :create]
   end
 end
