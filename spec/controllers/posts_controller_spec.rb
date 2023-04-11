6404ab4705da679e0b3d3acab3785d05eab98632
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-12 02:01:39 +0600

    Fix linter errors.

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
   let(:user) do
-    User.create(name: 'Shahadat Hossain', photo: 'https://images.unsplash.com/photo-1651684215020-f7a5b6610f23?&fit=crop&w=640', bio: 'A skilled carpenter with over 15 years of experience, specializing in custom furniture design and installation.') # Create a user with ID of 1
+    User.create(name: 'Shahadat Hossain', photo: 'https://images.unsplash.com/photo-1651684215020-f7a5b6610f23?&fit=crop&w=640',
+                bio: 'A skilled carpenter with over 15 years of experience.')
   end
 
   let(:post) do
-    user.posts.create(author: user, title: 'First seed post', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.') # Create a post for the user
+    user.posts.create(author: user, title: 'First seed post',
+                      text: 'Lorown printer took.')
   end
 
   describe 'GET #index' do
