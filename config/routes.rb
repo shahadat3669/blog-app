b2aed1f5ea9044761dcb66cc11f3a0b089d57862
Shahadat Hossain <shahadat3669@gmail.com>
2023-04-12 20:53:25 +0600

    Add device configuration file for users.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  
+  root to: "users#index"
+
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :likes, only: [:new, :create]
